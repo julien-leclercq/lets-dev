828e1e2d823d8c7e78782b3fb2c30357df490423
Sofiane Gargouri <contact@sofianeg.com>
2017-05-14 21:06:13 +0200

    Saving more user data on omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,15 @@ class User < ApplicationRecord
   has_many :desks, through: :desk_user_memberships
 
   def self.from_omniauth(auth)
+    puts auth.info
     u = where(email: auth.info.email).or(where("#{auth.provider}_id" => auth.uid)).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user["#{auth.provider}_id"] = auth.uid
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
+      user["#{auth.provider}_name"] = auth.info.name
+      user["#{auth.provider}_image"] = auth.info.image
+      user['github_username'] = auth.info.nickname if auth.provider == 'github'
+
       user.skip_confirmation!
     end
     u.update("#{auth.provider}_id" => auth.uid) if u["#{auth.provider}_id"].nil?
@@ -26,8 +27,8 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if (data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info'])
+        user.email = data['email'] if user.email.blank?
       end
     end
   end

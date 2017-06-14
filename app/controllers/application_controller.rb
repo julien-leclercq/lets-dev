1dab151f71ef31e15fef628e040991b908434b61
Sofiane Gargouri <contact@sofianeg.com>
2017-06-14 12:20:15 +0200

    Adding locale based on  header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,10 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = current_user.try(:locale) || cookies.try(:locale) || I18n.default_locale
+    I18n.locale = current_user.try(:locale) || cookies.try(:locale) || extract_locale_from_accept_language_header || I18n.default_locale
   end
 
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
